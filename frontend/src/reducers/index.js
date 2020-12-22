2e0e0a0c77143398ca8f445a37a02ccbc24e2795
Nicholas Siu <ncyk.siu@gmail.com>
2020-12-21 20:40:39 -0800

    combine reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,8 @@
 import {combineReducers} from "redux";
+import moviesReducer from "./moviesReducer";
+import nominationsReducer from "./nominationsReducer";
 
 export default combineReducers({
-
+  movieList: moviesReducer,
+  nominationList: nominationsReducer
 });
