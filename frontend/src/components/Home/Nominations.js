e072b4a271d79cbfacd1549d7c52418e7d279aee
Nicholas Siu <ncyk.siu@gmail.com>
2020-12-23 14:03:49 -0800

    extra nomination text

--- frontend/src/components/Home/Nominations.js
+++ frontend/src/components/Home/Nominations.js
@@ -5,7 +5,14 @@ import Nomination from "./Nomination";
 const Nominations = ({nominations}) => {
   return (
     <Fragment>
-      <h3 className="h1.display-4 my-3">Nominations</h3>
+      <h3 className="h1.display-4 my-3">
+        Nominations
+        <span>
+          <small className="form-text text-muted" style={{fontSize: "1rem"}}>
+            Limit 5
+          </small>
+        </span>
+      </h3>
       <ul className="list-group">
         {
           nominations.map((nomination) => 
@@ -14,7 +21,7 @@ const Nominations = ({nominations}) => {
         }
       </ul>
       <small className="form-text text-muted">
-        Nomination Count: {nominations.length}
+        Nomination Count: <strong>{nominations.length} out of 5</strong>
       </small>
     </Fragment>
   )
